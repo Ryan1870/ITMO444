bb2a0507fa8964c5497be1c7d09c20183a1a3553
Ryan Anderson <ryan.anderson558@gmail.com>
2015-10-26 03:07:05 -0500

    more file changes

---
+++ application/install-webserver.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+
+sudo apt-get update -y
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl
+
+git clone https://github.com/Ryan1870/ITMO444.git
+
+mv ./ITMO444/images /var/www/html/images
+mv ./ITMO444/index.html /var/www/html
+mv ./ITMO444/*php /var/www/html
+
+
+curl -sS https://getcomposer.org/installer | sudo php
+
+sudo php composer.phar require aws/aws-sdk-php
+
+sudo mv vendor /var/www/html
+
+echo "DOne!" > results.txt
+
+
+
