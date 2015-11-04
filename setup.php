2df04a33f35f01ef21ba96b7a4f883e937c470c3
Ryan Anderson <ryan.anderson558@gmail.com>
2015-11-03 21:57:32 -0600

    more files for db

--- setup.php
+++ setup.php
@@ -57,20 +57,20 @@ $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 print "============\n". $endpoint . "================\n";
 $link = mysqli_connect($endpoint,"controller","letmein888","3306") or die("Error " . mysqli_error($link)); 
 echo "Here is the result: " . $link;
-$sql = "CREATE TABLE comments 
-(
-ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-PosterName VARCHAR(32),
-Title VARCHAR(32),
-Content VARCHAR(500),
-uname Varchar(20),
-email Varchar(20),
-phone Varchar(20),
-s3URL Varchar(256),
-jpgfile Varchar(256),
-state TineInt(3),
-date Timestamp
+#$sql = "CREATE TABLE comments 
+#(
+#ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+#PosterName VARCHAR(32),
+#Title VARCHAR(32),
+#Content VARCHAR(500),
+#uname Varchar(20),
+#email Varchar(20),
+#phone Varchar(20),
+#s3URL Varchar(256),
+#jpgfile Varchar(256),
+#state TineInt(3),
+#date Timestamp
 
-)";
+#)";
 $con->query($sql);
 ?>
