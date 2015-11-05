1e87f5a099dd410ba585c26a3427fe22154a39cd
Ryan Anderson <ryan.anderson558@gmail.com>
2015-11-04 21:48:48 -0600

    lremove commas

--- dbcreate.php
+++ dbcreate.php
@@ -23,19 +23,19 @@ if($link === false){
 //echo "Here is the result: " . $link;
 
 
-$sql = "CREATE TABLE 'comments' 
+$sql = "CREATE TABLE comments
 (
-'ID' INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-'PosterName' VARCHAR(32),
-'Title' VARCHAR(32),
-'Content' VARCHAR(500),
-'uname' Varchar(20),
-'email' Varchar(20),
-'phone' Varchar(20),
-'s3URL' Varchar(256),
-'jpgfile' Varchar(256),
-'state' TineInt(3),
-'date' Timestamp
+ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+PosterName VARCHAR(32),
+Title VARCHAR(32),
+Content VARCHAR(500),
+uname Varchar(20),
+email Varchar(20),
+phone Varchar(20),
+s3URL Varchar(256),
+jpgfile Varchar(256),
+state TineInt(3),
+date Timestamp
 )";
 
 if (mysqli_query($link, $sql)){
