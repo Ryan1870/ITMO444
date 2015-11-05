a7bfb8e6ab4189206fc05cacd1fc10488f0306c1
Ryan Anderson <ryan.anderson558@gmail.com>
2015-11-04 21:28:03 -0600

    more db

--- dbcreate.php
+++ dbcreate.php
@@ -20,19 +20,19 @@ $link = mysqli_connect($endpoint,"controller","letmein888") or die("Error " . my
 //echo "Here is the result: " . $link;
 
 
-$sql = "CREATE TABLE comments 
+$sql = mysql_query("CREATE TABLE 'comments' 
 (
-ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-PosterName VARCHAR(32),
-Title VARCHAR(32),
-Content VARCHAR(500),
-uname Varchar(20),
-email Varchar(20),
-phone Varchar(20),
-s3URL Varchar(256),
-jpgfile Varchar(256),
-state TineInt(3),
-date Timestamp
+'ID' INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+'PosterName' VARCHAR(32),
+'Title' VARCHAR(32),
+'Content' VARCHAR(500),
+'uname' Varchar(20),
+'email' Varchar(20),
+'phone' Varchar(20),
+'s3URL' Varchar(256),
+'jpgfile' Varchar(256),
+'state' TineInt(3),
+'date' Timestamp
 )";
 
 ?>
