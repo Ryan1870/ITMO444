f7532a15a9bbff524e91ccd9c360928441ce7504
Ryan Anderson <ryan.anderson558@gmail.com>
2015-11-05 18:52:58 -0600

    change insert statment

--- result.php
+++ result.php
@@ -123,7 +123,8 @@ $issubscribed=0;
 
 mysqli_query($link, "INSERT INTO comments (ID, PosterName,Title,Content,uname,phone,s3URL,jpgfile,state,date) VALUES ('$email','$filename','$filename','$filename','$email','$phone','$s3rawurl','$uploadfile','$status')");
 
-
+$results = $link->insert_id;
+echo $results;
 #if( $statement !== FALSE){
 #	$statement->bind_param("ssssssssi",$email,$filename,$filename,$filename,$email,$phone,$s3rawurl,$uploadfile,$status);
 #	$statement->execute();
