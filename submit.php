80ef43e1c333aed3ef7d7f4a883468e584dac672
Ryan Anderson <ryan.anderson558@gmail.com>
2015-11-29 23:54:17 -0600

    finished now cleaning code"
    "

--- submit.php
+++ submit.php
@@ -316,7 +316,7 @@ if($num_rows[0] > 0){
 //if not subscribed then subscribe the user and UPDATE the column in the database with a new value 0 to 1 so that then each time you don't have to resubscribe them
 // add code to generate SQS Message with a value of the ID returned from the most recent inserted piece of work
 //  Add code to update database to UPDATE status column to 1 (in progress)
-	#header('Location: gallery.php'); 
+	header('Location: gallery.php'); 
 
 //Dynamically resize images
 function thumb_create($file, $width , $height ) {
